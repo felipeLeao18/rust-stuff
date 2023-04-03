d08784b13d3fbbf65ebe3114b8844c88da5f9d9a
felipeLeao18 <flpleao1807@gmail.com>
2023-04-03 19:33:57 -0300

    feat: finish part two

--- advent-of-code/day-one/src/main.rs
+++ advent-of-code/day-one/src/main.rs
@@ -1,10 +1,11 @@
-use std::{collections::HashMap, env, path::PathBuf};
+use std::collections::BTreeMap;
+use std::{env, path::PathBuf};
 
 fn main() {
     let path = PathBuf::from(env::current_dir().unwrap().join("src/input.txt"));
     let buffer: String = std::fs::read_to_string(path).unwrap();
 
-    let mut calories_by_elf: HashMap<i32, usize> = HashMap::new();
+    let mut calories_by_elf: BTreeMap<i32, usize> = BTreeMap::new();
 
     for (i, calories_group) in buffer.split_terminator("\n\n").enumerate() {
         let mut total_calories_by_elf: i32 = 0;
@@ -17,4 +18,12 @@ fn main() {
 
     let result: Option<&i32> = calories_by_elf.keys().max();
     println!("{:?}", result);
+
+    let top_three_sum: i32 = calories_by_elf
+        .keys()
+        .rev()
+        .take(3)
+        .fold(0, |acc, x| acc + x);
+
+    println!("{}", top_three_sum);
 }
